f70dfaf87bd3eb29cab60177af9046f304b317ae
hahalolha <93250867+hahalolha@users.noreply.github.com>
2025-11-13 11:27:50 +1100

    Update obk_config.h
    Add battery driver & remove unnecessary drivers for BL602, to increase free space in 1mb variants

--- src/obk_config.h
+++ src/obk_config.h
@@ -194,13 +194,13 @@
 #define ENABLE_TASMOTADEVICEGROUPS				1
 #define ENABLE_LITTLEFS							1
 #define ENABLE_NTP								1
-// #define ENABLE_NTP_DST						1
+#define ENABLE_NTP_DST						1
 #define ENABLE_CALENDAR_EVENTS					1
 #define ENABLE_DRIVER_LED						1
-#define ENABLE_DRIVER_BL0937					1
-#define ENABLE_DRIVER_BL0942					1
-#define ENABLE_DRIVER_CSE7766					1
-#define ENABLE_DRIVER_WEMO						1
+// #define ENABLE_DRIVER_BL0937					1
+// #define ENABLE_DRIVER_BL0942					1
+// #define ENABLE_DRIVER_CSE7766					1
+// #define ENABLE_DRIVER_WEMO						1
 #define ENABLE_DRIVER_FREEZE					0
 #define ENABLE_DRIVER_DHT						1
 // parse things like $CH1 or $hour etc
@@ -209,12 +209,14 @@
 #define ENABLE_DRIVER_DDP						1
 #define ENABLE_DRIVER_SSDP						1
 #define ENABLE_DRIVER_CHT83XX					1
-#define ENABLE_DRIVER_DS1820					1
+// #define ENABLE_DRIVER_DS1820					1
 #define ENABLE_OBK_SCRIPTING					1
 // #define ENABLE_I2C							1
 #define ENABLE_ADVANCED_CHANNELTYPES_DISCOVERY	1
-#define ENABLE_DRIVER_SM16703P					1
-#define ENABLE_DRIVER_PIXELANIM					1
+// #define ENABLE_DRIVER_SM16703P					1
+// #define ENABLE_DRIVER_PIXELANIM					1
+
+#define ENABLE_DRIVER_BATTERY					1
 
 #if (OBK_VARIANT == OBK_VARIANT_BERRY)
 #define ENABLE_OBK_BERRY						1
